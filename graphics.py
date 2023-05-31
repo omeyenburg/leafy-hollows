4fd0e4eec970bfbf62bd638cd9ebdcf669bae7b5
oskar <135001586+oskarmeyenburg@users.noreply.github.com>
2023-05-31 19:26:21 +0200

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -1,10 +1,9 @@
 import sys, os
+import scripts.shader as shader
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame.locals import *
 import pygame
-
-import shader
 
 
 def quit():
@@ -15,24 +14,27 @@ def quit():
 
 class Window:
     def __init__(self):
+        self.max_fps = 60
+        self.vsync = 0
+        self.events = None
+        
         info = pygame.display.Info()
         self.size = (int(info.current_w / 3 * 2), int(info.current_h / 5 * 3))
         self.width, self.height = self.size
-        self.window = shader.init(self.width, self.height, 0) # Shader connection
+        self.window = shader.init(self.width, self.height, self.vsync) # Shader connection
         self.clock = pygame.time.Clock()
-        self.max_fps = 60
-        self.events = None
 
     def resize(self, width, height):
         self.size = (width, height)
         self.width, self.height = self.size
-        self.window = shader.modify_window(self.width, self.height, 0) # Shader connection
+        self.window = shader.modify_window(self.width, self.height, self.vsync) # Shader connection
 
     def update(self, world_surface, ui_surface):
         shader.update(world_surface, ui_surface) # Shader connection
         
         self.clock.tick(self.max_fps)
         self.events = pygame.event.get()
+        
         for event in self.events:
             if event.type == pygame.QUIT:
                 quit()
