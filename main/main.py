fe9d458c84f87e2bd0880e43cdf58becb2ec9eaa
Jakob Röhrborn <jakob662006@gmail.com>
2023-06-07 08:54:50 +0200

    wfc implementation

--- main/main.py
+++ main/main.py
@@ -21,6 +21,57 @@ world_surface = pygame.Surface(window.size)
 ui_surface = pygame.Surface(window.size)
 font = graphics.Font(util.File.path("data/fonts/font.png"))
 
+#
+import time
+t0 = time.time()
+import wfc
+
+dirt = wfc.State(
+    "dirt",
+    wfc.Rule(
+        lambda x, y: {
+            (x, y+1): ["dirt", "grass"],
+            (x, y-1): ["stone", "dirt"]
+        }
+    )
+)
+
+stone = wfc.State(
+    "stone",
+    wfc.Rule(
+        lambda x, y : {
+            (x, y+1): ["stone", "dirt"]
+        }
+    )
+)
+
+air = wfc.State(
+    "air",
+    wfc.Rule(
+        lambda x, y : {
+            (x, y+1): ["air"]
+        }
+    )
+)
+
+landscape_wave = wfc.Wave(
+    (10, 70),
+    [dirt, stone, air]
+)
+landscape = landscape_wave.collapse()
+
+block_map = []
+for row in landscape:
+    block_map.append([item.state.name for item in row])
+
+block_to_color = {
+    "air": pygame.Color(255, 255, 255),
+    "dirt": pygame.Color(255,248,220),
+    "stone": pygame.Color(128, 128, 128)
+}
+
+print(f"wfc time: {time.time() - t0}")
+
 time = -1
 while True:
     time += 1
@@ -32,10 +83,16 @@ while True:
     ui_surface.fill((0, 0, 0))
 
     # Draw
+    """
     pygame.draw.circle(ui_surface, (255, 0, 0), (200, 200), 60)
     pos = pygame.mouse.get_pos()
     world_surface.blit(tree, pos)
-    font.write(ui_surface, str(window.clock.get_fps()), (255, 255, 0), 7, (100, 100))
+    """
+    for y in range(len(block_map)):
+        for x in range(len(block_map[0])):
+            pygame.draw.rect(world_surface, block_to_color[block_map[y][x]], (15*x, 15*y, 15, 15))
+    
+    font.write(ui_surface, str(window.clock.get_fps()), (255, 255, 0), 2, (0, 0))   # FPS Counter
 
     # Update window + shader
     window.update(world_surface, ui_surface)
