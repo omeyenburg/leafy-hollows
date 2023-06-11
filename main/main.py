e9fb593d3496877a6d70094078c97d612136e34c
Jakob Röhrborn <jakob662006@gmail.com>
2023-06-11 19:46:46 +0200

    player v0

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ import pygame
 
 
 # Create window
-window = graphics.Window("Test", use_opengl=False)
+window = graphics.Window("Test", use_opengl=False, keys=("w", "a", "s", "d"))
 world_surface, ui_surface = window.surfaces()
 
 # Create and activate shader
@@ -42,10 +42,25 @@ second_page.layout()
 b4.callback = second_page.open
 """
 
+class Player():
+    def __init__(self, spawn_pos, player_size) -> None:
+        self.pos = spawn_pos
+        self.player_size = player_size
+
+    def update(self):
+        print(window.keys)
+        self.draw()
+
+    def draw(self):
+        pygame.draw.rect(world_surface, (0,255,0), pygame.Rect((self.pos[0], self.pos[1]), (self.player_size[0], self.player_size[1])))
+
+
 import scripts.map_generator as map_generator
-world_width, world_height = 10, 10
+world_width, world_height = 12, 6
 world_blocks = map_generator.default_states(world_width, world_height)
 blocks_to_color = {"air":(255,255,255), "dirt":(255,248,220), "stone":(128,128,128)}
+
+player = Player(spawn_pos=[100, 100], player_size=[50, 100])
 
 time = -1
 while True:
@@ -64,12 +79,14 @@ while True:
     menu.update()
 
     # drawing blocks
-    block_width, block_height = 10, 10   # scale to window
+    block_width, block_height = 100, 100   # scale to window
     for y in range(len(world_blocks)):
         for x in range(len(world_blocks[0])):
             pygame.draw.rect(world_surface, blocks_to_color[world_blocks[y][x]], (block_width*x, block_height*y, block_width, block_height))
     
     font.write(ui_surface, str(window.clock.get_fps()), (255, 0, 0), 2, (0, 0))   # FPS Counter
+    
+    player.update()
 
     # Update window + shader
     window.update()
