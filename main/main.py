9ad8292c393f12249427cee0325070f24c84cbc0
Jakob Röhrborn <jakob662006@gmail.com>
2023-06-14 13:01:09 +0200

    improved movement & cleanup

--- main/main.py
+++ main/main.py
@@ -12,14 +12,6 @@ import math
 window = graphics.Window("Test", use_opengl=False, keys=("w", "a", "s", "d"))
 world_surface, ui_surface = window.surfaces()
 
-# Create and activate shader
-vert = util.File.path("data/shaders/template.vert")
-frag = util.File.path("data/shaders/wave.frag")
-shader = graphics.shader.Shader(vert, frag, time="int")
-shader.activate()
-
-# Pygame stuff for testing
-tree = pygame.image.load("data/images/tree.jpg").convert()
 font = graphics.Font(util.File.path("data/fonts/font.png"))
 
 class Physics_Object():
@@ -32,19 +24,18 @@ class Physics_Object():
         self.vel: list[float, float] = [0.0, 0.0]
 
     
-    def apply_force(self, force: float, angle: int):    # angle in degrees; 0 straight up, clockwise
+    def apply_force(self, force: float, angle: int):    # angle in degrees; 0 right, counterclockwise
         angle = math.radians(angle)
-        self.vel[0] += ((math.sin(angle) * force) / self.mass) * delta_time
-        self.vel[1] += ((-math.cos(angle) * force) / self.mass) * delta_time
+        self.vel[0] += ((math.cos(angle) * force) / self.mass) * delta_time
+        self.vel[1] += -((math.sin(angle) * force) / self.mass) * delta_time
     
     def update(self):
         self.pos[0] += self.vel[0] * delta_time
         self.pos[1] += self.vel[1] * delta_time
 
-
 class Player(Physics_Object):
     def __init__(self, spawn_pos: list[float, float], size: int, speed: int) -> None:
-        Physics_Object.__init__(self, 100, True, 0, spawn_pos)
+        Physics_Object.__init__(self, 100, False, 0, spawn_pos)
 
         #self.pos: list[float, float] = spawn_pos
         #self.vel: list[float, float] = [0.0, 0.0]
@@ -52,30 +43,71 @@ class Player(Physics_Object):
         self.speed: int = speed
 
     def draw(self):
-        pygame.draw.rect(world_surface, (0,255,0), pygame.Rect((self.pos[0] - (self.size[0] // 2), self.pos[1] - (self.size[1] // 2)), (self.size[0], self.size[1])))
+        #pygame.draw.rect(world_surface, (0,255,0), pygame.Rect((self.pos[0] - (self.size[0] // 2), self.pos[1] - (self.size[1] // 2)), (self.size[0], self.size[1])))
+        player_rect = pygame.Rect((0,0), self.size)
+        player_rect.center = self.pos
+        pygame.draw.rect(world_surface, (0,255,0), player_rect)
+
+        pygame.draw.line(ui_surface, (255, 0, 0), self.pos, (window.mouse_pos[0], window.mouse_pos[1]), width=1)
 
     def move(self):
-        keys = window.keys
-        force = 1000
-        if keys["w"] > 0:
-            #self.vel[1] = -self.speed
-            self.apply_force(force, 0)
-        if keys["a"] > 0:
-            #self.vel[0] = -self.speed
-            self.apply_force(force, 270)
-        if keys["s"] > 0:
-            #self.vel[1] = self.speed 
-            self.apply_force(force, 180)
-        if keys["d"] > 0:
-            #self.vel[0] = self.speed 
-            self.apply_force(force, 90)
+        def mouse_pull():
+            mouse_pos = [window.mouse_pos[0], window.mouse_pos[1]]
+        
+            dx, dy = mouse_pos[0] - self.pos[0], self.pos[1] - mouse_pos[1]
+            angle_to_mouse = math.degrees(math.atan2(dy, dx))
 
+            force = math.dist(self.pos, mouse_pos) * (10**4)
+
+            self.apply_force(force, angle_to_mouse)
+
+        keys = window.keys
+        d_speed = self.speed * delta_time
+
+        if keys["w"] > 0:
+            if self.vel[1] > 0:
+                if  self.vel[1] < d_speed:
+                    self.vel[1] = 0
+                else:
+                    self.vel[1] -= d_speed
+            else:
+                self.pos[1] -= d_speed
+
+        if keys["a"] > 0:
+            if self.vel[0] > 0:
+                if  self.vel[0] < d_speed:
+                    self.vel[0] = 0
+                else:
+                    self.vel[0] -= d_speed
+            else:
+                self.pos[0] -= d_speed
+
+        if keys["s"] > 0:
+            if self.vel[1] < 0:
+                if  self.vel[1] > d_speed:
+                    self.vel[1] = 0
+                else:
+                    self.vel[1] += d_speed
+            else:
+                self.pos[1] += d_speed
+
+        if keys["d"] > 0:
+            if self.vel[0] < 0:
+                if  self.vel[0] > d_speed:
+                    self.vel[0] = 0
+                else:
+                    self.vel[0] += d_speed
+            else:
+                self.pos[0] += d_speed
 
         if window.mouse_buttons[0] == 1:
-            self.apply_force(force, 0)
+            mouse_pull()
+
+        if self.gravity:
+            self.apply_force((9.81 * 50) * self.mass, 270) # 50px = 1m
 
     def update(self):
-        print(self.pos, self.vel)
+        #print(self.pos, self.vel)
         Physics_Object.update(self)
         self.move()
         self.draw()
@@ -86,27 +118,17 @@ world_width, world_height = 12, 6
 world_blocks = map_generator.default_states(world_width, world_height)
 blocks_to_color = {"air":(255,255,255), "dirt":(255,248,220), "stone":(128,128,128)}
 
-player = Player(spawn_pos=[100, 100], size=[50, 100], speed=250)
-
-time = -1
+player = Player(spawn_pos=[window.width / 2, window.height / 2], size=[50, 100], speed=250)
 
 delta_time = 0
 while True:
     delta_time = (1 / window.clock.get_fps()) if window.clock.get_fps() > 0 else delta_time
 
     world_surface, ui_surface = window.surfaces()
-
-    # Send variables to the fragment shader
-    time += 1
-    time += window.mouse_wheel[3] * 10 # y-axis-scroll
-    shader.setvar("time", time)
     
     # Reset surfaces
     world_surface.fill((0, 0, 0))
     ui_surface.fill((0, 0, 0))
-
-    # Draw and update menu
-    menu.update()
 
     # drawing blocks
     block_width, block_height = 100, 100   # scale to window
