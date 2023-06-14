98a6fbc2e69323be2752a68ee6c68b1ed05c8ab8
Jakob Röhrborn <jakob662006@gmail.com>
2023-06-14 20:01:17 +0200

    refactor to rect based movement

--- main/main.py
+++ main/main.py
@@ -17,59 +17,60 @@ font = graphics.Font(util.File.path("data/fonts/font.png"))
 PIXELS_PER_METER = 50
 
 class Physics_Object():
-    def __init__(self, mass: int, gravity: bool, hitbox, position: list[float, float]) -> None:
+    def __init__(self, mass: int, gravity: bool, rect: pygame.Rect, position: list[float, float]) -> None:
         self.mass: int = mass
         self.gravity: bool = gravity
-        self.hitbox = hitbox
 
-        self.pos: list[float, float] = position
+        self.__pos: list[float, float] = position     # pos used for setting, rect used for reading
+        print(position)
         self.vel: list[float, float] = [0.0, 0.0]
+        self.rect: pygame.Rect = rect
 
-    
+    def move_position(self, mov: list[float, float]):
+        self.__pos[0] += mov[0]
+        self.__pos[1] += mov[1]
+
     def apply_force(self, force: float, angle: int):    # angle in degrees; 0 right, counterclockwise
         angle = math.radians(angle)
         self.vel[0] += ((math.cos(angle) * force) / self.mass) * delta_time
         self.vel[1] += -((math.sin(angle) * force) / self.mass) * delta_time
     
     def update(self):
+        
         if self.gravity:
             self.apply_force((9.81 * PIXELS_PER_METER) * self.mass, 270)
 
-        if self.pos[1] + player.size[1] // 2 >= window.height:  # on the ground (temp)
+        
+        if self.rect.bottom >= window.height:  # on the ground (temp)
             if self.vel[1] > 0:
                 self.vel[1] = 0
+        
+        self.move_position([vel * delta_time for vel in self.vel])     # move by frame velocity
 
-        self.pos[0] += self.vel[0] * delta_time
-        self.pos[1] += self.vel[1] * delta_time
+        self.rect.center = self.__pos
+
 
 
 class Player(Physics_Object):
-    def __init__(self, spawn_pos: list[float, float], size: list[int, int], speed: float, jump_force: float) -> None:   # stats in SI-Units
-        Physics_Object.__init__(self, 100, True, 0, spawn_pos)
-
-        #self.pos: list[float, float] = spawn_pos
-        #self.vel: list[float, float] = [0.0, 0.0]
-        self.size: list[int, int] = size
+    def __init__(self, spawn_pos: list[float, float], speed: float, jump_force: float) -> None:   # stats in SI-Units
+        Physics_Object.__init__(self, 100, True, pygame.Rect((0, 0), (50, 100)), spawn_pos)
 
         self.speed: float = speed * PIXELS_PER_METER
         self.jump_force: float = jump_force * PIXELS_PER_METER
 
     def draw(self):
-        #pygame.draw.rect(world_surface, (0,255,0), pygame.Rect((self.pos[0] - (self.size[0] // 2), self.pos[1] - (self.size[1] // 2)), (self.size[0], self.size[1])))
-        player_rect = pygame.Rect((0,0), self.size)
-        player_rect.center = self.pos
-        pygame.draw.rect(world_surface, (0,255,0), player_rect)
+        pygame.draw.rect(world_surface, (0,255,0), self.rect)
 
-        pygame.draw.line(ui_surface, (255, 0, 0), self.pos, (window.mouse_pos[0], window.mouse_pos[1]), width=1)
+        pygame.draw.line(ui_surface, (255, 0, 0), self.rect.center, (window.mouse_pos[0], window.mouse_pos[1]), width=1)
 
     def move(self):
         def mouse_pull():
             mouse_pos = [window.mouse_pos[0], window.mouse_pos[1]]
         
-            dx, dy = mouse_pos[0] - self.pos[0], self.pos[1] - mouse_pos[1]
+            dx, dy = mouse_pos[0] - self.rect.centerx, self.rect.centery - mouse_pos[1]
             angle_to_mouse = math.degrees(math.atan2(dy, dx))
 
-            force = math.dist(self.pos, mouse_pos) * (10**4)
+            force = math.dist(self.rect.center, mouse_pos) * (10**4)
 
             self.apply_force(force, angle_to_mouse)
 
@@ -83,7 +84,7 @@ class Player(Physics_Object):
                 else:
                     self.vel[1] -= d_speed
             else:
-                self.pos[1] -= d_speed
+                self.rect.y -= d_speed
 
         if keys["a"] > 0:
             if self.vel[0] > 0:
@@ -92,7 +93,7 @@ class Player(Physics_Object):
                 else:
                     self.vel[0] -= d_speed
             else:
-                self.pos[0] -= d_speed
+                self.move_position([-d_speed, 0])
 
         if keys["s"] > 0:
             if self.vel[1] < 0:
@@ -101,7 +102,7 @@ class Player(Physics_Object):
                 else:
                     self.vel[1] += d_speed
             else:
-                self.pos[1] += d_speed
+                self.rect.y += d_speed
 
         if keys["d"] > 0:
             if self.vel[0] < 0:
@@ -110,7 +111,7 @@ class Player(Physics_Object):
                 else:
                     self.vel[0] += d_speed
             else:
-                self.pos[0] += d_speed
+                self.move_position([d_speed, 0])
 
         if keys["space"] == 1:
             jump_duration = 0.5   # how long is jump force applied --> variable jump height
@@ -121,7 +122,8 @@ class Player(Physics_Object):
             mouse_pull()    # constant activation balances out w/ gravity --> usable as rope
 
     def update(self):
-        #print(self.pos, self.vel)
+        #print(self.rect.center, self.vel)
+
         Physics_Object.update(self)
         self.move()
         self.draw()
@@ -130,9 +132,9 @@ class Player(Physics_Object):
 import scripts.map_generator as map_generator
 world_width, world_height = 12, 6
 world_blocks = map_generator.default_states(world_width, world_height)
-blocks_to_color = {"air":(255,255,255), "dirt":(255,248,220), "stone":(128,128,128)}
+blocks_to_color = {"air":(0,0,0), "dirt":(255,248,220), "stone":(128,128,128)}
 
-player = Player(spawn_pos=[window.width / 2, window.height / 2], size=[50, 100], speed=5, jump_force=1000)
+player = Player(spawn_pos=[window.width / 2, window.height / 2], speed=5, jump_force=1000)
 
 delta_time = 0
 while True:
