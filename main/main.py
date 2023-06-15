db933fdd76259653b5e2f9a58a2716e1887f4e4b
Jakob Röhrborn <jakob662006@gmail.com>
2023-06-15 16:32:37 +0200

    movement completly buggy

--- main/main.py
+++ main/main.py
@@ -21,20 +21,42 @@ class Physics_Object():
         self.mass: int = mass
         self.gravity: bool = gravity
 
-        self.__pos: list[float, float] = position     # pos used for setting, rect used for reading
-        print(position)
+        self.pos: list[float, float] = position     # pos used for setting, rect used for reading
         self.vel: list[float, float] = [0.0, 0.0]
         self.rect: pygame.Rect = rect
-
-    def move_position(self, mov: list[float, float]):
-        self.__pos[0] += mov[0]
-        self.__pos[1] += mov[1]
 
     def apply_force(self, force: float, angle: int):    # angle in degrees; 0 right, counterclockwise
         angle = math.radians(angle)
         self.vel[0] += ((math.cos(angle) * force) / self.mass) * delta_time
         self.vel[1] += -((math.sin(angle) * force) / self.mass) * delta_time
     
+    def horizontal_collide(self):
+        for rect in terrain:
+            if self.rect.colliderect(rect):
+                print(self.vel)
+                if self.vel[0] < 0:
+                    self.rect.left = rect.right
+                    self.pos[0] = self.rect.centerx
+                    self.vel[0] = 0
+
+                elif self.vel[0] > 0:
+                    self.rect.right = rect.left
+                    self.pos[0] = self.rect.centerx
+                    self.vel[0] = 0
+    
+    def vertical_collide(self):
+        for rect in terrain:
+            if self.rect.colliderect(rect):
+                if self.vel[1] < 0:
+                    self.rect.top = rect.bottom
+                    self.pos[1] = self.rect.centerx
+                    self.vel[1] = 0
+
+                elif self.vel[1] > 0:
+                    self.rect.bottom = rect.top
+                    self.pos[1] = self.rect.centerx
+                    self.vel[1] = 0
+
     def update(self):
         
         if self.gravity:
@@ -45,9 +67,14 @@ class Physics_Object():
             if self.vel[1] > 0:
                 self.vel[1] = 0
         
-        self.move_position([vel * delta_time for vel in self.vel])     # move by frame velocity
+        self.pos[0] += self.vel[0] * delta_time
+        self.horizontal_collide()
+        self.pos[1] += self.vel[1] * delta_time
+        self.vertical_collide()
+        #self.move_position([vel * delta_time for vel in self.vel])     # move by frame velocity
+        
 
-        self.rect.center = self.__pos
+        self.rect.center = self.pos
 
 
 
@@ -58,10 +85,15 @@ class Player(Physics_Object):
         self.speed: float = speed * PIXELS_PER_METER
         self.jump_force: float = jump_force * PIXELS_PER_METER
 
+        self.mov_flags: list[bool, bool] = [False, False]   # 0d, 1a
+
     def draw(self):
         pygame.draw.rect(world_surface, (0,255,0), self.rect)
 
         pygame.draw.line(ui_surface, (255, 0, 0), self.rect.center, (window.mouse_pos[0], window.mouse_pos[1]), width=1)
+    
+    def jump(self, duration: float):
+        self.apply_force(self.jump_force * (duration / delta_time), 90)
 
     def move(self):
         def mouse_pull():
@@ -77,49 +109,43 @@ class Player(Physics_Object):
         keys = window.keys
         d_speed = self.speed * delta_time
 
-        if keys["w"] > 0:
-            if self.vel[1] > 0:
-                if  self.vel[1] < d_speed:
-                    self.vel[1] = 0
-                else:
-                    self.vel[1] -= d_speed
-            else:
-                self.rect.y -= d_speed
+        #if keys["w"] > 0:
+        #    self.vel[1] = -self.speed
+        
+        #if keys["s"] > 0:
+        #    self.vel[1] = self.speed
 
+        if self.mov_flags[0]:
+            self.vel[0] -= self.speed
+            self.mov_flags[0] = False
+        if keys["d"] > 0:
+            self.vel[0] += self.speed
+            self.mov_flags[0] = True
+        
+        if self.mov_flags[1]:
+            self.vel[0] += self.speed
+            self.mov_flags[1] = False
         if keys["a"] > 0:
-            if self.vel[0] > 0:
+            self.vel[0] -= self.speed
+            self.mov_flags[1] = True
+
+        if keys["space"] == 1:
+            self.jump(0.5)   # how long is jump force applied --> variable jump height
+            
+
+
+        if window.mouse_buttons[0] == 1:
+            mouse_pull()    # constant activation balances out w/ gravity --> usable as rope
+
+        """
+        if self.vel[0] > 0:
                 if  self.vel[0] < d_speed:
                     self.vel[0] = 0
                 else:
                     self.vel[0] -= d_speed
             else:
-                self.move_position([-d_speed, 0])
-
-        if keys["s"] > 0:
-            if self.vel[1] < 0:
-                if  self.vel[1] > d_speed:
-                    self.vel[1] = 0
-                else:
-                    self.vel[1] += d_speed
-            else:
-                self.rect.y += d_speed
-
-        if keys["d"] > 0:
-            if self.vel[0] < 0:
-                if  self.vel[0] > d_speed:
-                    self.vel[0] = 0
-                else:
-                    self.vel[0] += d_speed
-            else:
-                self.move_position([d_speed, 0])
-
-        if keys["space"] == 1:
-            jump_duration = 0.5   # how long is jump force applied --> variable jump height
-            self.apply_force(self.jump_force * (jump_duration / delta_time), 90)
-
-
-        if window.mouse_buttons[0] == 1:
-            mouse_pull()    # constant activation balances out w/ gravity --> usable as rope
+                self.pos[0] -= d_speed 
+        """
 
     def update(self):
         #print(self.rect.center, self.vel)
@@ -136,6 +162,8 @@ blocks_to_color = {"air":(0,0,0), "dirt":(255,248,220), "stone":(128,128,128)}
 
 player = Player(spawn_pos=[window.width / 2, window.height / 2], speed=5, jump_force=1000)
 
+terrain: list[pygame.Rect] = []
+
 delta_time = 0
 while True:
     delta_time = (1 / window.clock.get_fps()) if window.clock.get_fps() > 0 else delta_time
@@ -147,10 +175,13 @@ while True:
     ui_surface.fill((0, 0, 0))
 
     # drawing blocks
-    block_width, block_height = 100, 100   # scale to window
+    block_width, block_height = 90, 90   # scale to window
     for y in range(len(world_blocks)):
         for x in range(len(world_blocks[0])):
-            pygame.draw.rect(world_surface, blocks_to_color[world_blocks[y][x]], (block_width*x, block_height*y, block_width, block_height))
+            block_rect = pygame.Rect(block_width*x, block_height*y, block_width, block_height)
+            if world_blocks[y][x] != "air":
+                terrain.append(block_rect)
+            pygame.draw.rect(world_surface, blocks_to_color[world_blocks[y][x]], block_rect)
     
     font.write(ui_surface, str(window.clock.get_fps()), (255, 0, 0), 2, (0, 0))   # FPS Counter
     
