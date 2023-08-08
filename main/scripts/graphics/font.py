f81aabc6f5c1a5617ccd0e8215ee01cab45734c4
oskarmeyenburg <oskar.meyenburg@gmail.com>
2023-08-08 16:44:24 +0200

    More font characters

--- main/scripts/graphics/font.py
+++ main/scripts/graphics/font.py
@@ -2,87 +2,40 @@
 import pygame
 
 
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789äÄüÜöÖß _.,:;?!<=>#@\'\"+-*/()"
+
+
 class Font:
-    def fromPNG(path):
-        """
-        Load a monospaced font from a PNG file with all letters from chr(32) to chr(96).
-        """
-        image = pygame.image.load(path).convert()
+    def __new__(cls, name=None, size=1, bold=False, antialias=False):
+        char_rects = {}
 
-        letter_width = image.get_width() // 64
-        letter_height = image.get_height()
-        letters = {chr(i + 32): (1 / 64 * i, 1 / 64, letter_height / image.get_width()) for i in range(64)}
-
-        return (letters, image)
-
-    def fromTTF(path, size=1, antialias=False, lower=True):
-        """
-        Load a font from a TrueTypeFont file.
-        """
-        font = pygame.font.Font(path, size)
-        images = []
-        letters = {}
-        if lower: # upper letters :96 | lower letters :123
-            limit = 123
-        else:
-            limit = 96
-
-        font_height = font.render("".join([chr(i) for i in range(32, limit)]), antialias, (0, 0, 0)).get_height()
-        font_width = 0
-
-        space = font.render("A", antialias, (0, 0, 0))
-
-        for i in range(32, limit):
-            letter = chr(i)
-            if letter != " ":
-                image = font.render(letter, antialias, (255, 255, 255))
-            else:
-                image = space
-            letter_width = image.get_width()
-            letters[chr(i)] = (font_width, letter_width, font_height)
-
-            font_width += letter_width
-            images.append(image)
-
-        image = pygame.Surface((font_width, font_height))
-
-        for letter in letters:
-            image.blit(images[ord(letter) - 32], (0, letters[letter][0]))
-            letters[letter] = (letters[letter][0] / font_width, letters[letter][1] / font_width, font_height / font_width)
-
-        return (letters, image)
-
-    def fromSYS(name, size=1, bold=False, antialias=False, lower=True):
-        """
-        Load a font from the system.
-        """
         font = pygame.font.SysFont(name, size, bold=bold)
         images = []
-        letters = {}
-        if lower: # upper letters 32:96 | upper & lower letters 32:123
-            limit = 123
-        else:
-            limit = 96
-
-        font_height = font.render("".join([chr(i) for i in range(32, limit)]), antialias, (0, 0, 0)).get_height()
+        font_height = font.render(characters, antialias, (0, 0, 0)).get_height()
         font_width = 0
-
         space = font.render("A", antialias, (0, 0, 0))
-        for i in range(32, limit):
-            letter = chr(i)
-            if letter != " ":
-                image = font.render(letter, antialias, (255, 255, 255))
+
+        for char in characters:
+            if char != " ":
+                image = font.render(char, antialias, (255, 255, 255))
             else:
                 image = space
-            letter_width = image.get_width()
-            letters[chr(i)] = (font_width, letter_width, font_height)
+            char_width = image.get_width()
+            char_rects[char] = (font_width, char_width, font_height)
 
-            font_width += letter_width
+            font_width += char_width
             images.append(image)
 
         image = pygame.Surface((font_width, font_height))
-        for letter in letters:
-            image.blit(images[ord(letter) - 32], (letters[letter][0], 0))
-            letters[letter] = (letters[letter][0] / font_width, letters[letter][1] / font_width, font_height / font_width)
+        instance = super().__new__(cls)
+        instance.char_rects = char_rects
+        for i, char in enumerate(characters):
+            image.blit(images[i], (instance.char_rects[char][0], 0))
+            instance.char_rects[char] = (instance.char_rects[char][0] / font_width, instance.char_rects[char][1] / font_width, font_height / font_width)
 
-        return (letters, image)
+        return instance, image
+
+    def get_rect(self, char):
+        if not char in characters:
+            char = "?"
+        return self.char_rects[char]
