6847c6d2f4cdf99db59ecae3e117fd7e3a95d0ee
oskarmeyenburg <oskar.meyenburg@gmail.com>
2023-08-10 17:29:10 +0200

    Changing hitbox for crouching & sprint animation

--- main/scripts/game/physics.py
+++ main/scripts/game/physics.py
@@ -26,11 +26,19 @@ class CollisionPhysicsObject:
         self.onWallRight: int = 0
 
     def apply_force(self, force: float, angle: float, delta_time: float): # angle in degrees; 0 is right, counterclockwise
+        """
+        Applies force to the object.
+        """
         r_angle = math.radians(angle)
         self.vel[0] += math.cos(r_angle) * force / self.mass * delta_time
         self.vel[1] += math.sin(r_angle) * force / self.mass * delta_time
     
     def apply_velocity(self, world, delta_time: float):
+        """
+        Applies velocity to the object.
+        """
+        last_position = self.rect.topleft
+
         self.rect.x += self.vel[0] * delta_time
         self.rect.x = round(self.rect.x, 5) # bugs occur at higher precision
         self.x_collide(world, delta_time)
@@ -39,15 +47,30 @@ class CollisionPhysicsObject:
         self.rect.y = round(self.rect.y, 5)
         self.y_collide(world)
 
+        if self.get_collision(world):
+            self.rect.topleft = last_position
+            self.vel = [0, 0]
+
+    def get_collision(self, world):
+        """
+        Returns whether the object collides with a block.
+        """
         for x in range(math.floor(self.rect.left), math.ceil(self.rect.right)):
             for y in range(math.floor(self.rect.top), math.ceil(self.rect.bottom)):
                 if world.get_collision_block(x, y):
-                    ...#print("unresolved collision:", (x, y))
+                    return True
+        return False
 
     def gravity(self, delta_time):
+        """
+        Applies gravity to the object.
+        """
         self.apply_force(GRAVITY_CONSTANT * self.mass * delta_time, 270, 1)
     
     def x_collide(self, world, delta_time):
+        """
+        Resolves collisions on the x-axis.
+        """
         for x in range(math.floor(round(self.rect.left, 5)), math.ceil(round(self.rect.right, 5))):
             for y in range(math.floor(round(self.rect.top, 5)), math.ceil(round(self.rect.bottom, 5))):
                 if world.get_collision_block(x, y):
@@ -62,6 +85,9 @@ class CollisionPhysicsObject:
                         self.onWallLeft = max(2, int(WALL_JUMP_THRESHOLD / delta_time))
     
     def y_collide(self, world):
+        """
+        Resolves collisions on the y-axis.
+        """
         for x in range(math.floor(round(self.rect.left, 5)), math.ceil(round(self.rect.right, 5))):
             for y in range(math.floor(round(self.rect.top, 5)), math.ceil(round(self.rect.bottom, 5))):
                 if world.get_collision_block(x, y):
@@ -76,6 +102,9 @@ class CollisionPhysicsObject:
                         self.onGround = JUMP_THRESHOLD
 
     def update(self, world, delta_time):
+        """
+        Moves the object.
+        """
         self.gravity(delta_time)
         self.apply_force(delta_time * abs(world.wind) / (self.mass), 90 + 90 * min(1, max(-1, -world.wind)), self.mass)
         if self.onGround:
@@ -101,11 +130,17 @@ class PhysicsObject: # No collision
         self.gravity_constant = gravity
 
     def apply_force(self, force: float, angle: float, delta_time: float): # angle in degrees; 0 is right, counterclockwise
+        """
+        Applies force to the object.
+        """
         r_angle = math.radians(angle)
         self.vel[0] += math.cos(r_angle) * force / self.mass * delta_time
         self.vel[1] += math.sin(r_angle) * force / self.mass * delta_time
 
     def update(self, world, delta_time):
+        """
+        Moves the object.
+        """
         self.apply_force(self.gravity_constant * self.mass * delta_time, 270, 1)
         self.apply_force(delta_time * abs(world.wind) / (self.mass / 5), 90 + 90 * min(1, max(-1, -world.wind)), self.mass)
         self.rect.x += self.vel[0] * delta_time
