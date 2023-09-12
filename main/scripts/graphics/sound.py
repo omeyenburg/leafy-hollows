f138daccbc843ed332a329612ae317698e2cd044
oskarmeyenburg <oskar.meyenburg@gmail.com>
2023-09-12 21:09:31 +0200

    New sounds

--- main/scripts/graphics/sound.py
+++ main/scripts/graphics/sound.py
@@ -20,11 +20,17 @@ def load():
     for path in sound_paths:
         data = file.read(path, file_format="json")
         name = data["name"]
-        speed = data["speed"]
+        delay = data["speed"]
         volume = data["volume"]
         category = data["category"]
         files = tuple([pygame.mixer.Sound(sound_files[sound_file]) for sound_file in data["sounds"]])
-        loaded_sounds[name] = (speed, volume, files, category)
+        if isinstance(delay, (int, float)):
+            delay_range = 0
+        else:
+            delay = min(delay)
+            delay_range = max(delay) - min(delay)
+
+        loaded_sounds[name] = (delay, delay_range, volume, files, category)
 
     return loaded_sounds, played_sounds
 
@@ -37,15 +43,19 @@ def play(window, sound: str, x: float=0.0, identifier: str=None, channel_volume:
     loaded_sounds = window.loaded_sounds
     played_sounds = window.played_sounds
 
-    delay, volume, files, category = loaded_sounds[sound]
+    delay, delay_range, volume, files, category = loaded_sounds[sound]
 
     if delay != 0.0:
         if identifier is None:
             key = sound
         else:
             key = sound + identifier
+
         if key in played_sounds and played_sounds[key] > window.time:
             return # sound blocked
+        
+        if delay_range:
+            delay = delay + delay_range * random.random()
         played_sounds[key] = window.time + delay
 
     if len(files) > 1:
@@ -64,8 +74,11 @@ def play(window, sound: str, x: float=0.0, identifier: str=None, channel_volume:
         side = (1, 1)
 
     options_volume = window.options[category + " volume"] * window.options["master volume"]
-
+    if not volume * options_volume:
+        return
+    
     files[index].set_volume(volume * options_volume)
     channel = pygame.mixer.find_channel()
-    channel.play(files[index], fade_ms=100)
-    channel.set_volume(*side)
+    if not channel is None:
+        channel.play(files[index], fade_ms=100)
+        channel.set_volume(*side)
